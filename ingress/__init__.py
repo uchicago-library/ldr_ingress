e1b6ddd43cbc55e5482f9cdd299ccf3aa6c95b25
brian <balsamo@uchicago.edu>
2017-02-20 13:55:59 -0600

    removing debugging config settings from app.

--- ingress/__init__.py
+++ ingress/__init__.py
@@ -3,9 +3,6 @@ from .blueprint import BLUEPRINT
 
 app = Flask(__name__)
 
-app.config['PREMIS_ENDPOINT'] = "http://127.0.0.1:8910/"
-app.config['MATERIALSUITE_ENDPOINT'] = "http://127.0.0.1:8911/add"
-app.config['ACCS_ENDPOINT'] = "http://127.0.0.1:8912/"
 app.config.from_envvar("INGRESS_SETTINGS", silent=True)
 
 app.register_blueprint(BLUEPRINT)
