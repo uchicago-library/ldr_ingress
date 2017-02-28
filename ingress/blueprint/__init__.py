fb58474f63fe65932af9811eec1325f309d11d52
brian <balsamo@uchicago.edu>
2017-02-28 10:43:38 -0600

    Scratch that, that functionality should be in the other microservice...

--- ingress/blueprint/__init__.py
+++ ingress/blueprint/__init__.py
@@ -250,15 +250,9 @@ class Ingress(Resource):
         <input type="submit" value="Submit">
         </div>
         </form>
-        <form action={} method="post" style="margin: 0; padding: 0;">
-		<p>
-		Mint an Accession
-		<input style="display: inline;" type="submit" value="Mint" />
-		</p>
-		</form>
     </body>
 </html>
-""".format(BLUEPRINT.config.get("ACCS_ENDPOINT"))
+"""
         return output_html(resp, 200)
 
 
